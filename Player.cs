d5021dc3d4d97c800f51e1eda088d8025cc52a78
Seagull44 <56669320+Seagull44@users.noreply.github.com>
2020-06-05 09:39:21 +0300

    Platformer code

---
+++ Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float horisontalSpeed;
+    float speedX;
+    public float verticalImpulse;
+    Rigidbody2D rb;
+    bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            speedX = -horisontalSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            speedX = horisontalSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, verticalImpulse), ForceMode2D.Impulse);
+        }
+        transform.Translate(speedX, 0, 0);
+        speedX = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Ground")
+
+            isGrounded = true;
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+
+            isGrounded = false;
+
+    }
+}
